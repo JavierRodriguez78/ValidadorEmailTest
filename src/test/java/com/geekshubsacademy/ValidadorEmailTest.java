8bb0bcd0de0d8f27a0d4977b73b0ade0f96ddd3e
xavi <xavi@geekshubs.com>
2019-10-27 19:02:41 +0100

    first commit

---
+++ src/test/java/com/geekshubsacademy/ValidadorEmailTest.java
@@ -0,0 +1,21 @@
+package com.geekshubsacademy;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Escribe tests para indagar cómo de bien funciona
+ * la clase ValidadorEmail que está en este mismo paquete.
+ * @author random
+ *
+ */
+public class ValidadorEmailTest {
+
+
+        @Test
+        public void test() {
+            fail("Not yet implemented");
+        }
+
+
+}
